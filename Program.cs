2deb6897e3a7f6ef5f6dae90ec0e11a1f80c1fb5
Brad Culberson <brad.culberson@snowflake.com>
2023-12-15 08:24:35 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+string underscoreUri = "https://sfsenorthamerica_demo226.snowflakecomputing.com/";
+string dashUri = "https://sfsenorthamerica-demo226.snowflakecomputing.com/";
+
+async Task CheckHostCertificate(string uri)
+{
+    var handler = new HttpClientHandler();
+    var client = new HttpClient(handler);
+    try
+    {
+        using (var msg = new HttpRequestMessage(HttpMethod.Get, uri))
+        {
+            using (var response = await client.SendAsync(msg))
+            {
+                Console.WriteLine($"Hostname {uri} is ok");
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Hostname {uri} is NOT ok {ex}");
+    }
+}
+
+await CheckHostCertificate(underscoreUri);
+await CheckHostCertificate(dashUri);
---
+++ README.md
@@ -0,0 +1,13 @@
+# underscoredTls
+
+To build the project in docker:
+
+```sh
+docker build . -t underscoredTls
+```
+
+To run the project in docker
+
+```sh
+docker run underscoredTls
+```
